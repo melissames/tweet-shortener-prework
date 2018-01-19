4abb5f6082dec1671ecf93828e492867f25b09f9
Melissa Sames <melissa.sames@gmail.com>
2018-01-19 21:04:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -50,8 +50,8 @@ def shortened_tweet_truncator (tweet)
   shortened_tweet = word_substituter(tweet)
 
   if shortened_tweet > 140
-    "#{tweet[0..139]}..."
+    "#{tweet[0..136]}..."
   else
     tweet
   end
-end 
+end
