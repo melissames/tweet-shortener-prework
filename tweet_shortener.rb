6f6eb3c9beedeb3a2c128e62a403b951ab82a7e6
Melissa Sames <melissa.sames@gmail.com>
2018-01-19 21:04:33 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,3 +45,13 @@ def selective_tweet_shortener (tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator (tweet)
+  shortened_tweet = word_substituter(tweet)
+
+  if shortened_tweet.length > 140
+    "#{tweet[0..136]}..."
+  else
+    tweet
+  end
+end
