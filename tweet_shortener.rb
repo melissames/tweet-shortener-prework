39c4a21a505519bd3fe013edd58e714855e6658e
Melissa Sames <melissa.sames@gmail.com>
2018-01-19 20:44:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&'
+  }
+end
+
+def word_substituter (tweet)
+  tweet_array = tweet.split(' ')
+  dictionary_keys = dictionary.keys
+  word_intersection = dictionary_keys & tweet_array
+  shortened_tweet = ''
+
+  tweet_array.each { |word|
+    if word_intersection.include?(word)
+      shortened_tweet << "#{dictionary[word]} "
+    else
+      shortened_tweet << "#{word} "
+    end
+  }
+
+
+  shortened_tweet[0..-2]
+end
